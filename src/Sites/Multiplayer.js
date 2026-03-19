f28a628a7e81e78ade7c925451bd201b018cec72
Eder03 <konstei009@gmail.com>
2026-03-19 20:10:15 +0100

    Update 1.5

--- src/Sites/Multiplayer.js
+++ src/Sites/Multiplayer.js
@@ -73,8 +73,8 @@ export default class ApiForm extends Component {
 
     };
 
-    //this.socket = io('localhost:5002');
-    this.socket = io('https://vmq-server.onrender.com');
+    this.socket = io('localhost:5002');
+    //this.socket = io('https://vmq-server.onrender.com');
     this.startGame = this.startGame.bind(this);
   
     this.handleUsernameSubmit = this.handleUsernameSubmit.bind(this);
